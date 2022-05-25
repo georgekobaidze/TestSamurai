6ce28da2364e757df6102058cd0ce1d339cfdbee
George Kobaidze <george.kobaidze.14@gmail.com>
2022-05-25 22:46:21 +0400

    Add NUnit tests

---
+++ TestSamurai.NUnitTests/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+
+namespace TestSamurai.NUnitTests
+{
+    [TestFixture]
+    public class ReservationTests
+    {
+        [Test]
+        public void CanBeCancelledBy_Admin_ReturnsTrue()
+        {
+            var reservation = new Reservation();
+            var admin = new User { IsAdmin = true };
+
+            var canBeCancelledByAdmin = reservation.CanBeCancelledBy(admin);
+
+            Assert.That(canBeCancelledByAdmin, Is.True);
+        }
+
+        [Test]
+        public void CanBeCancelledBy_MakerUser_ReturnsTrue()
+        {
+            var reservation = new Reservation();
+            var user = new User { IsAdmin = true };
+
+            reservation.MadeBy = user;
+
+            var canBeCancelledByMakerUser = reservation.CanBeCancelledBy(user);
+
+            Assert.That(canBeCancelledByMakerUser, Is.True);
+        }
+
+        [Test]
+        public void CanBeCancelledBy_RegularUser_ReturnsFalse()
+        {
+            var reservation = new Reservation();
+            var user = new User { IsAdmin = false };
+
+            var canBeCancelledByRegularUser = reservation.CanBeCancelledBy(user);
+
+            Assert.That(canBeCancelledByRegularUser, Is.False);
+        }
+    }
+}
