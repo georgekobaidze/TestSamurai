2516eeb7f2f39bf4f34c3f4374dcf95bcdef866a
George Kobaidze <george.kobaidze.14@gmail.com>
2022-06-05 13:12:08 +0400

    Add FizzBuzz tests

---
+++ TestSamurai.NUnitTests/FizzBuzzTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+
+namespace TestSamurai.NUnitTests;
+
+[TestFixture]
+public class FizzBuzzTests
+{
+    [Test]
+    public void GetOutput_DivisibleByThreeOnly_StartsWithFizz()
+    {
+        var number = 3;
+        var result = FizzBuzz.GetOutput(number);
+
+        Assert.That(result, Is.EqualTo("Fizz"));
+    }
+
+    [Test]
+    public void GetOutput_DivisibleByFiveOnly_EndsWithBuzz()
+    {
+        var number = 5;
+        var result = FizzBuzz.GetOutput(number);
+
+        Assert.That(result, Is.EqualTo("Buzz"));
+    }
+
+    [Test]
+    public void GetOutput_DivisibleByThreeAndFive_ReturnsFizzBuzz()
+    {
+        var number = 15;
+        var result = FizzBuzz.GetOutput(number);
+
+        Assert.That(result, Is.EqualTo("FizzBuzz"));
+    }
+
+    [Test]
+    public void GetOutput_NonDivisibleByThreeOrFive_ReturnsNumber()
+    {
+        var number = 7;
+        var result = FizzBuzz.GetOutput(number);
+
+        Assert.That(result, Is.EqualTo(number.ToString()));
+    }
+}
