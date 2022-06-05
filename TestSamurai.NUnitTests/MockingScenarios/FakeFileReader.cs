0cca17b9ca42c385d077342753d445ce4b81c230
George Kobaidze <george.kobaidze.14@gmail.com>
2022-06-05 22:20:51 +0400

    Replace manually created moq model with the moq model created by Moq library

--- TestSamurai.NUnitTests/MockingScenarios/FakeFileReader.cs
+++ TestSamurai.NUnitTests/MockingScenarios/FakeFileReader.cs
@@ -1,7 +1,7 @@
 ﻿using TestSamurai.MockingScenarios;
 
 namespace TestSamurai.NUnitTests.MockingScenarios;
-internal class FakeFileReader : IFileReader
+internal class FakeFileReader : IFileReader // Creating mock classes manually isn't the best practice, it's better to use mock libraries, like Moq
 {
     public string Read()
     {
--- TestSamurai.NUnitTests/MockingScenarios/VideoServiceTests.cs
+++ TestSamurai.NUnitTests/MockingScenarios/VideoServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
 using TestSamurai.MockingScenarios;
 
 namespace TestSamurai.NUnitTests.MockingScenarios;
@@ -6,11 +7,21 @@ namespace TestSamurai.NUnitTests.MockingScenarios;
 [TestFixture]
 public class VideoServiceTests
 {
+    private VideoService _videoService;
+
+    [SetUp]
+    public void SetUp()
+    {
+        var fileReader = new Mock<IFileReader>();
+        fileReader.Setup(fr => fr.Read()).Returns("");
+
+        _videoService = new VideoService(fileReader.Object);
+    }
+
     [Test]
     public void ReadVideoTitle_VideoIsEmpty_ResultContainsError()
     {
-        var videoService = new VideoService(new FakeFileReader());
-        var result = videoService.ReadVideoTitle();
+        var result = _videoService.ReadVideoTitle();
 
         Assert.That(result, Does.Contain("error").IgnoreCase);
     }
